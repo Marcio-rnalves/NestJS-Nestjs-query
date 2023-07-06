199dfaf0a0f923e4a0acba966db8df2899d3fcbd
Marcio Alves <marcio.rnalves@gmail.com>
2023-07-06 20:48:29 -0300

    atualização network

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Olá Mundo! Trabalho final de Engenharia de Software 2!';
   }
 }
